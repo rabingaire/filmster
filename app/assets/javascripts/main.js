ca7e2e1593c25d600a1ffd76b9487960d7c25a92
Rabin Gaire <rabingaire20@gmail.com>
2016-11-26 22:56:35 +0545

    more info of movie can be seen now

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,21 @@
-
 $(function(){
   $.ajax({
     url: 'http://www.omdbapi.com/?',
     data: {"s": "superman"}
   })
   .done(function(data){
+    displayMovies(data);
+  });
+
+  function displayMovies(data){
     let htmlString = "";
 
     data["Search"].forEach(function(movie){
-      htmlString += `<p>${movie["Title"]}</p>`;
+    htmlString += `<img src=${movie["Poster"]} />
+                   <p>${movie["Title"]}</p>
+                   <p>${movie["Year"]}</p>`;
     });
-    
+
     $("#movies").append(htmlString);
-  });
-})
+  }
+});
